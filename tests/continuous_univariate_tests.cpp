37b7496499912e56ccffd2756e5feb18a3a09763
Fergus Cooper <fcooper8472@gmail.com>
2019-10-31 23:15:09 +0000

    Add placeholder for discrete uniform distributions

--- tests/continuous_univariate_tests.cpp
+++ tests/continuous_univariate_tests.cpp
@@ -22,8 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-// This tells Catch to provide a main() - only do this in one cpp file
-#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
 #include <limits>
